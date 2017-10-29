628c76872af23914413c8026528b913c9ea037ee
UBUNTU <ubuntu@ubuntu-pc>
2017-10-29 23:49:04 +1100

    add black checkout, shopping cart, product-small, product, home, ... components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,22 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { mainReducer } from "./state-management/reducers/main-reducer";
 import { StoreModule } from "@ngrx/store";
+import { ProductSmallComponent } from './product-small/product-small.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductSmallComponent,
+    ProductComponent,
+    CategoryComponent,
+    HomeComponent,
+    ShoppingCartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
